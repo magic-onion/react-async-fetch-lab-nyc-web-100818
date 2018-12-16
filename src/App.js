86e1a21538da8f118d6df8438c336f5e494378af
magic-onion <alexander.m.truong@gmail.com>
2018-12-16 12:10:51 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, {Component} from 'react'
+import ExampleComponent from './components/ExampleComponent'
+import Button from './components/Button'
+import Greeting from './components/Greeting'
+
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(r=>r.json())
+    .then(p=>console.log(p))
+  }
+
+  render() {
+    return (
+      <div>
+      <ExampleComponent />
+      <Button />
+      <Greeting />
+      </div>
+    )
+  }
+}
